5f460d378fea510b697534e0811171e576db0571
JemimaReine <reinejemima05@gmail.com>
2024-02-11 15:30:15 +0000

    bug connexion

--- server.py
+++ server.py
@@ -26,8 +26,18 @@ def index():
 
 @app.route('/showSummary',methods=['POST'])
 def showSummary():
-    club = [club for club in clubs if club['email'] == request.form['email']][0]
-    return render_template('welcome.html',club=club,competitions=competitions)
+    email = request.form.get('email')
+    if email:  # Vérifiez si un email a été soumis
+        club = [club for club in clubs if club['email'] == email]
+        if not club:
+            flash('Veuillez entrer un email valide.', 'error')
+            return redirect(url_for('index'))  # Redirige vers la page d'accueil
+        club = club[0]
+        return render_template('welcome.html', club=club, competitions=competitions)
+    else:
+        flash('Veuillez entrer un email.', 'error')
+        return redirect(url_for('index'))  # Redirige vers la page d'accueil
+
 
 
 @app.route('/book/<competition>/<club>')
